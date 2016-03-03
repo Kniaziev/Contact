18065e61674c095553af2b93647f009253a69bff
Kniaziev <knyazev.alexandr@gmail.com>
2016-03-03 15:54:43 +0200

    abc

---
+++ Contact/src/Hello.java
@@ -0,0 +1,28 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import javax.xml.soap.SOAPPart;
+
+/**
+ * Created by kniaziev on 3/3/2016.
+ */
+public class Hello {
+
+
+        public static void main(String[] args) {
+            int line = 2;
+            switch(line){
+                case 1:
+                    System.out.println("Вот дом, Который построил Джек.");
+                case 2:
+                    if (line>1){
+
+                    System.out.println("А это пшеница, Которая в темном чулане хранится");
+                    }
+                case 3:
+
+
+            }
+
+        }
+
+    }
+
